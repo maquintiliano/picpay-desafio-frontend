22f55689128ac182c0f815e8ef70d67c2825d713
vicabats <vitoria_batistoti@hotmail.com>
2022-02-14 17:41:09 -0300

    Fix paginator

--- src/domains/payment/components/table/table.component.ts
+++ src/domains/payment/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { Payment } from 'src/domains/payment/models/Payment';
@@ -9,24 +9,29 @@ import { Payment } from 'src/domains/payment/models/Payment';
   styleUrls: ['./table.component.scss']
 })
 
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, AfterViewInit {
   @Input() data: Payment[]
   @Output() onPaymentChange = new EventEmitter<Payment>();
   @Output() onEdit = new EventEmitter<Payment>();
   @Output() onDelete = new EventEmitter<Payment>();
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatTable, { static: true }) table: MatTable<any>;
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  // @ViewChild(MatTable, { static: true }) table: MatTable<any>;
 
   public displayedColumns: string[] = ['name', 'title', 'date', 'value', 'isPayed', 'edit'];
-  public dataSource;
+  public dataSource: MatTableDataSource<any>;
 
   public showOptions: boolean = false;
 
-  constructor() { }
+  constructor() {
+    this.dataSource = new MatTableDataSource(this.data)
+  }
 
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<Payment>(this.data || []);
+  }
+
+  ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
   }
 
